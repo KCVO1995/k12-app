092301108dc57dd6b73d161650accc39c5c44f45
李彦辉 <943514589@qq.com>
2024-05-11 22:25:25 +0800

    feat: 安装 antd

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,5 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
@@ -15,6 +19,28 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
+    plugins: [
+      vue(),
+      // 自动引入
+      AutoImport({
+        // imports: ['vue', 'vue-router'],
+        // dirs: ['./src/renderer/src/api'],
+        // dts: './src/renderer/src/auto-imports.js',
+        resolvers: [
+          ElementPlusResolver({
+            // 自动引入修改主题色添加这一行，使用预处理样式，不添加将会导致使用ElMessage，ElNotification等组件时默认的主题色会覆盖自定义的主题色
+            importStyle: 'sass'
+          })
+        ]
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver({
+            // 自动引入修改主题色添加这一行，使用预处理样式
+            importStyle: 'sass'
+          })
+        ]
+      })
+    ]
   }
 })
