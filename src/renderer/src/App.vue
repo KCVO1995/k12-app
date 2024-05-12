f9d167ded5fe328d2e9731649ac7d86ee16c4767
李彦辉 <943514589@qq.com>
2024-05-12 17:47:34 +0800

    style: 文件转化基础 UI

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,20 +1,60 @@
 <script setup lang="ts">
+import iconCSV from '@renderer/assets/imgs/icon-csv.png'
+import iconTransform from '@renderer/assets/imgs/icon-transform.png'
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
   <div class="global">
     <div class="sku-settings"></div>
-    <div class="control"></div>
-    <el-button @click="ipcHandle">账号密码登录</el-button>
+    <div class="control">
+      <div class="left file">
+        <img class="icon" :src="iconCSV" alt="" />
+        <div class="file-name">2011.csv</div>
+      </div>
+      <div class="middle">
+        <img class="icon-transform" :src="iconTransform" alt="" />
+      </div>
+      <div class="right file">
+        <img class="icon" :src="iconCSV" alt="" />
+        <div class="file-name">2011.csv</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .global {
+  padding: 60px;
+
+  .sku-settings {
+    border: 1px solid green;
+    height: 250px;
+  }
+
   .control {
-    border: 1px solid red;
-    height: 500px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    width: 700px;
+    height: 300px;
+
+    .icon-transform {
+      width: 50px;
+      height: 50px;
+    }
+
+    .file {
+      .file-name {
+        text-align: center;
+      }
+
+      .icon {
+        width: 150px;
+        height: 150px;
+      }
+    }
   }
 }
 </style>
